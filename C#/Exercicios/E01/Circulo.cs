62db850ce3d69b1030d8269ca5fb8c746472abb0
anderson-henriqq <144293720+anderson-henriqq@users.noreply.github.com>
2024-07-03 17:27:53 +0000

    Atualização

---
+++ C#/Exercicios/E01/Circulo.cs
@@ -0,0 +1,12 @@
+class Circulo {
+    public double r;
+
+    public double area(){
+        return Math.PI * (r*r);
+    }
+
+    public double circun(){
+        return 2 * Math.PI * r;
+    }
+        
+}
---
+++ C#/Exercicios/E01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Circulo x = new Circulo();
+Console.WriteLine("Digite o raio:");
+x.r = double.Parse(Console.ReadLine()); // O readline ler tudo como string, double.Parse foi necessario para converter string em double
+Console.WriteLine(x.area());
+Console.WriteLine(x.circun());
+
+
+
+
